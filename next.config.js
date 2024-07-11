2497c511b601fb9d235201f5763e35372626cfe2
lerenfeld <34092125+lerenfeld@users.noreply.github.com>
2024-07-11 22:07:54 +0300

    init

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wrappbiz-general-bucket.s3.eu-north-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
